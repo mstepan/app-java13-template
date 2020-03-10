e37ac29bbe1de03e16db620435ed2952d9a0ba00
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-03-10 12:14:04 +0200

    AdditionChainTest completed.

--- src/main/java/com/max/app/Main.java
+++ src/main/java/com/max/app/Main.java
@@ -1,15 +1,10 @@
 package com.max.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public final class Main {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-
-        LOG.info("Main done...");
+        System.out.printf("java: %s%n", System.getProperty("java.version"));
     }
 }
