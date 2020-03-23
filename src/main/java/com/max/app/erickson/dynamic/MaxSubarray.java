73622766c4fa0c06f596acc555cc7fc48b7ebbaa
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-03-23 15:31:13 +0200

    max subarray product algorithm added, also algorithm use BigInteger to prevent any overflow/underflow errors during multiplication

--- src/main/java/com/max/app/erickson/dynamic/MaxSubarray.java
+++ src/main/java/com/max/app/erickson/dynamic/MaxSubarray.java
@@ -1,11 +1,93 @@
 package com.max.app.erickson.dynamic;
 
+import java.math.BigInteger;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
 public final class MaxSubarray {
 
     private MaxSubarray() {
         throw new AssertionError("Can't instantiate utility only class");
+    }
+
+    /**
+     * Find max subarray product using dynamic programming approach.
+     * <p>
+     * N = arr.length
+     * <p>
+     * time: O(N), if we neglect the fact that BigInteger multiplication is not O(1)
+     * space: O(1)
+     */
+    public static BigInteger findMaxProduct(int[] arr) {
+        checkArgument(arr != null, "null 'arr' argument passed");
+
+        //Use BigInteger here to prevent overflow/underflow during multiplication
+        BigInteger maxSoFar = BigInteger.ZERO;
+
+        BigInteger positive = BigInteger.ZERO;
+        BigInteger negative = BigInteger.ZERO;
+
+        for (int value : arr) {
+
+            if (value == 0) {
+                positive = BigInteger.ZERO;
+                negative = BigInteger.ZERO;
+            }
+            else if (value > 0) {
+                if (BigInteger.ZERO.equals(positive)) {
+                    positive = BigInteger.valueOf(value);
+                }
+                else {
+                    positive = positive.multiply(BigInteger.valueOf(value));
+                }
+
+                negative = negative.multiply(BigInteger.valueOf(value));
+            }
+            // value < 0
+            else {
+                if (BigInteger.ZERO.equals(negative)) {
+                    negative = (BigInteger.ZERO.equals(positive) ?
+                            BigInteger.valueOf(value) : positive.multiply(BigInteger.valueOf(value)));
+                    positive = BigInteger.ZERO;
+                }
+                else {
+
+                    BigInteger newPositive = negative.multiply(BigInteger.valueOf(value));
+
+                    negative = (BigInteger.ZERO.equals(positive) ?
+                            BigInteger.valueOf(value) : positive.multiply(BigInteger.valueOf(value)));
+                    positive = newPositive;
+                }
+            }
+
+            maxSoFar = maxSoFar.compareTo(positive) >= 0 ? maxSoFar : positive;
+        }
+
+        return maxSoFar;
+    }
+
+    /**
+     * Find max subarray product using bruteforce approach.
+     * <p>
+     * time: O(N^2), if we neglect the fact that BigInteger multiplication is not O(1)
+     * space: O(1)
+     */
+    public static BigInteger findMaxProductBruteforce(int[] arr) {
+        checkArgument(arr != null, "null 'arr' argument passed");
+
+        //Use BigInteger here to prevent overflow/underflow during multiplication
+        BigInteger maxSoFar = BigInteger.ZERO;
+
+        for (int i = 0; i < arr.length; ++i) {
+            BigInteger curSum = BigInteger.ONE;
+
+            for (int j = i; j < arr.length; ++j) {
+                curSum = curSum.multiply(BigInteger.valueOf(arr[j]));
+                maxSoFar = maxSoFar.compareTo(curSum) >= 0 ? maxSoFar : curSum;
+            }
+        }
+
+        return maxSoFar;
     }
 
     /**
--- src/test/java/com/max/app/erickson/dynamic/MaxSubarrayTest.java
+++ src/test/java/com/max/app/erickson/dynamic/MaxSubarrayTest.java
@@ -2,15 +2,44 @@ package com.max.app.erickson.dynamic;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Random;
 
+import static com.max.app.erickson.dynamic.MaxSubarray.findMaxProduct;
+import static com.max.app.erickson.dynamic.MaxSubarray.findMaxProductBruteforce;
 import static com.max.app.erickson.dynamic.MaxSubarray.findMaxSum;
 import static com.max.app.erickson.dynamic.MaxSubarray.findMaxSumBruteforce;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class MaxSubarrayTest {
+
+    @Test
+    public void findMaxProductNormalCase() {
+        int[] arr = {-6, 12, -7, 0, 14, -7, 5};
+
+        assertThat(findMaxProduct(arr)).
+                as("dynamic programming 'findMaxProduct' failed").
+                isEqualTo(new BigInteger("504"));
+
+        assertThat(findMaxProductBruteforce(arr)).
+                as("bruteforce 'findMaxProductBruteforce' failed").
+                isEqualTo(new BigInteger("504"));
+    }
+
+    @Test
+    public void findMaxProductCheckForRandomArrays() {
+        for (int it = 0; it < 100; ++it) {
+            int[] arr = generateRandomArray(100);
+
+            assertThat(findMaxProduct(arr)).
+                    as("dynamic programming for subarray PRODUCT is different from bruteforce, array %s",
+                       Arrays.toString(arr)).
+                    isEqualTo(findMaxProductBruteforce(arr));
+
+        }
+    }
 
     @Test
     public void findMaxSumNormalCase() {
@@ -24,20 +53,19 @@ public class MaxSubarrayTest {
     public void findMaxSumCheckForRandomArrays() {
 
         for (int it = 0; it < 1000; ++it) {
-            int[] arr = generateRandomArray();
+            int[] arr = generateRandomArray(100);
 
-            assertThat(findMaxSum(arr)).as("dynamic programming values is different from bruteforce for array %s",
+            assertThat(findMaxSum(arr)).as("dynamic programming for subarray SUM is different from bruteforce, array %s",
                                            Arrays.toString(arr)).
                     isEqualTo(findMaxSumBruteforce(arr));
 
         }
-
     }
 
     private static final Random RAND = new Random();
 
-    private static int[] generateRandomArray() {
-        int[] arr = new int[10 + RAND.nextInt(1000)];
+    private static int[] generateRandomArray(int maxExpectedLength) {
+        int[] arr = new int[Math.min(10 + RAND.nextInt(maxExpectedLength), maxExpectedLength)];
 
         for (int i = 0; i < arr.length; ++i) {
             arr[i] = -1000 + RAND.nextInt(2000);
