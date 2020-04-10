804a2c785e5cf4554e8fa2aa76303469fc9018d2
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-04-10 15:23:48 +0300

    thread pool size calculator added

--- src/main/java/com/max/app/Main.java
+++ src/main/java/com/max/app/Main.java
@@ -1,97 +1,12 @@
 package com.max.app;
 
-import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
-import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public final class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        createDeadlock();
 
-        long i = 0;
-        while( true ) {
-            SoftReference ref = new SoftReference<>(new CachedData(i));
-            ++i;
-        }
-
-
-//        System.out.printf("java: %s%n", System.getProperty("java.version"));
+        System.out.printf("java: %s%n", System.getProperty("java.version"));
     }
 
-    private static final class CachedData {
-
-        private final long index;
-
-
-        public CachedData(long index) {
-            this.index = index;
-        }
-
-        @Override
-        public void finalize() throws Throwable{
-            try {
-                System.out.println("Cached data deleted: " + index);
-            }
-            finally {
-                super.finalize();
-            }
-        }
-
-    }
-
-    public static void createDeadlock() {
-
-        CountDownLatch reached = new CountDownLatch(1);
-
-        Lock lock2 = new ReentrantLock();
-        Lock lock1 = new ReentrantLock();
-
-        try (CloseableLock notUsed = new CloseableLock(lock1)) {
-            new Thread(() -> {
-
-                try (CloseableLock notUsed2 = new CloseableLock(lock2)) {
-
-                    reached.countDown();
-
-                    try(CloseableLock notUsed4 = new CloseableLock(lock1)){
-                        System.out.printf("%s done%n", Thread.currentThread().getName());
-                    }
-
-                }
-            }).start();
-
-            System.out.println("Waiting");
-            reached.await();
-
-            try (CloseableLock notUsed3 = new CloseableLock(lock2)) {
-                System.out.printf("%s done%n", Thread.currentThread().getName());
-            }
-
-        }
-        catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private static final class CloseableLock implements AutoCloseable {
-
-        private final Lock impl;
-
-        public CloseableLock(Lock impl) {
-            this.impl = Objects.requireNonNull(impl);
-            this.impl.lock();
-        }
-
-        @Override
-        public void close() {
-            impl.unlock();
-        }
-    }
 
 }
---
+++ src/main/java/com/max/app/ThreadPoolSizeCalculatorMain.java
@@ -0,0 +1,24 @@
+package com.max.app;
+
+public final class ThreadPoolSizeCalculatorMain {
+
+    public static void main(String[] args) throws Exception {
+
+        int cpus = Runtime.getRuntime().availableProcessors();
+
+        // 70%, can be up to 75%
+        double desiredUtilization = 0.70;
+
+        // usually we wait 90% of time and only 10% used as computation time
+        double waitToComputeRatio = 90.0 / 10.0;
+
+        int threadsCount = (int) (cpus * desiredUtilization * (1.0 + waitToComputeRatio));
+
+        System.out.printf("CPUs: %d%n", cpus);
+        System.out.printf("threads count: %d%n", threadsCount);
+
+        System.out.printf("java: %s%n", System.getProperty("java.version"));
+    }
+
+
+}
