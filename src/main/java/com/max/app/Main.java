25dd468cc568f3076be947a0b4a1f109376a5027
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-04-12 10:50:05 +0300

    Dynamic programming solution for `shortest basic arithmetic expression` added.

--- src/main/java/com/max/app/Main.java
+++ src/main/java/com/max/app/Main.java
@@ -1,9 +1,20 @@
 package com.max.app;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Main {
 
     public static void main(String[] args) throws Exception {
 
+        Set<String> set = new HashSet<>();
+        set.add("first");
+        set.add("second");
+        set.add("third");
+
+        for (String str : set) {
+            System.out.println(str);
+        }
 
         System.out.printf("java: %s%n", System.getProperty("java.version"));
     }
